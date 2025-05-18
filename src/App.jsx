740e6678e48cf6c9835904869a79832ec35f991c
simonelupone <simone.lupone@gmail.com>
2025-05-18 23:15:49 +0200

    feat: add form structure and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
 const App = () => {
-  return (
-    <div className="text-red-500 text-3xl font-bold">Hello World!</div>
-  )
+
+    return (
+        <form className="mx-auto mt-10 p-6">
+            <div className="relative z-0 w-full mb-5 group">
+                <input type="text" name="title" id="title"
+                       className="block py-2.5 px-0 w-full text-lg text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                       placeholder=" " required/>
+                <label htmlFor="title"
+                       className="peer-focus:font-medium absolute text-lg text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Title</label>
+            </div>
+
+            <div className="relative z-0 w-full mb-5 group">
+                <input type="text" name="author" id="author"
+                       className="block py-2.5 px-0 w-full text-lg text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                       placeholder=" " required/>
+                <label htmlFor="author"
+                       className="peer-focus:font-medium absolute text-lg text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Author</label>
+            </div>
+
+
+            <label htmlFor="body" className="block mb-2 text-lg font-medium text-gray-900 dark:text-white">Body</label>
+            <textarea id="body" rows="4"
+                      className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-neutral-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="Write body here..."></textarea>
+
+            <button type="submit"
+                    className="mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit
+            </button>
+        </form>
+
+    )
 }
 
 export default App
