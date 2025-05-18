a483ada1ea584d3c0b51b64c1fc3a89a14537842
simonelupone <simone.lupone@gmail.com>
2025-05-18 23:43:26 +0200

    feat: add inputs onChange and values

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import {useState} from "react";
+
 const App = () => {
+
+    const [formData, setFormData] = useState({
+            title: '',
+            author: '',
+            body: '',
+            public: false
+        }
+    )
 
     return (
         <form className="mx-auto mt-10 p-6">
             <div className="relative z-0 w-full mb-5 group">
                 <input type="text" name="title" id="title"
+                       value={formData.title}
+                       onChange={(e) => {
+                           setFormData({...formData, title: e.target.value})
+                       }}
                        className="block py-2.5 px-0 w-full text-lg text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                        placeholder=" " required/>
                 <label htmlFor="title"
@@ -12,6 +26,10 @@ const App = () => {
 
             <div className="relative z-0 w-full mb-5 group">
                 <input type="text" name="author" id="author"
+                       value={formData.author}
+                       onChange={(e) => {
+                           setFormData({...formData, author: e.target.value})
+                       }}
                        className="block py-2.5 px-0 w-full text-lg text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                        placeholder=" " required/>
                 <label htmlFor="author"
@@ -19,13 +37,23 @@ const App = () => {
             </div>
 
             <div className="flex items-center mb-4">
-                <input id="checkbox" type="checkbox" value=""
+                <input id="public" type="checkbox"
+                       checked={formData.public}
+                       onChange={(e) => {
+                           setFormData({...formData, public: e.target.checked})
+                       }}
                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
-                <label htmlFor="checkbox" className="ms-2 text-lg font-medium text-gray-900 dark:text-gray-400">Public</label>
+                <label htmlFor="public"
+                       className="ms-2 text-lg font-medium text-gray-900 dark:text-gray-400">Public</label>
             </div>
 
-            <label htmlFor="body" className="block mb-2 text-lg font-medium text-gray-900 dark:text-gray-400">Body</label>
+            <label htmlFor="body"
+                   className="block mb-2 text-lg font-medium text-gray-900 dark:text-gray-400">Body</label>
             <textarea id="body" rows="4"
+                      value={formData.body}
+                      onChange={(e) => {
+                          setFormData({...formData, body: e.target.value})
+                      }}
                       className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-neutral-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                       placeholder="Write body here..."></textarea>
 
